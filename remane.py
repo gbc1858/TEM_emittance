8ec7396f91db48241c8125e0aad1e3f0060c2543
gbc1858 <scientisttomato@gmail.com>
2020-06-26 16:34:26 -0500

    remove relabeling file

--- remane.py
+++
@@ -1,6 +0,0 @@
-import os
-
-
-for file in os.listdir("/Users/chen/Desktop/github/TEM_emittance/enittance_1_06262020/"):
-    if file.endswith(".raw"):
-        os.rename("/Users/chen/Desktop/github/TEM_emittance/enittance_1_06262020/" + file, "/Users/chen/Desktop/github/TEM_emittance/enittance_1_06262020/" + '0' + file)
